038a552eb387aa73b6cf517d518e922a3dcc25ed
Davi-Ga <davigaldinoky@gmail.com>
2023-01-05 18:09:55 -0300

    dunder

--- dunder_methods/main.py
+++ dunder_methods/main.py
@@ -1,14 +1,48 @@
 class Pessoa:
     def __init__(self):
         self.pessoas=['Ana', 'Maria', 'João']
-
+        
+    def __str__(self):
+        return f'Pessoa(pessoas={self.pessoas})'
+    
+    def __getitem__(self, index):
+        return self.pessoas[index]
+    
+    def __len__(self):
+        x=0
+        while True:
+            try:
+                y=self.pessoas[x]
+                x+=1
+            except:
+                break    
+        return x
+    
+    def __add__(self,p2):
+        return self.pessoas+p2
+    
+    def __mul__(self,n):
+        return self.pessoas*n
+    
+    def __delitem__(self, index):
+        del self.pessoas[index]
+        
+    def __reversed__(self):
+        return [self.pessoas[i] for i in range(len(self.pessoas)-1,-1,-1)]
+        #return self.pessoas[::-1]
+        
     def __eq__(self, p2):
         if len(self.pessoas) == len(p2):
             x=[self.pessoas[i] == p2[i] for i in range(len(self.pessoas))]
             return all(x)
         else:
             return False
+    
+    def __iter__(self):
+        i=0
+        while i< len(self.pessoas):
+            yield self.pessoas[i]
+            i+=1
         
 x = Pessoa()
-y=Pessoa()
-print(x==y)
+print(reversed(x))
