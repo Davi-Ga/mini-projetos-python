12b19569ec173a47ba95778d12b64738a2c1b397
Davi-Ga <davigaldinoky@gmail.com>
2023-01-11 17:11:50 -0300

    funcs

--- manipulando-funcoes/func.py
+++ manipulando-funcoes/func.py
@@ -1,5 +1,33 @@
-from functools import singledispatch
-#decorator
+from functools import singledispatch,singledispatchmethod,partial
+
 @singledispatch
 def func(args):
     pass
+
+@func.register
+def _(args: int):
+    print("Inteiro chamado")
+    
+@func.register
+def _(args: str):
+    print("String chamada")
+    
+    
+class NeuralNetwork:
+    
+    @singledispatchmethod
+    def neg(self, args):
+        raise NotImplementedError('Not implemented')
+    
+    @neg.register
+    def _(self, args: int):
+        return -args
+    
+    @neg.register
+    def _(self, args: bool):
+        return not args
+    
+def mult(x,y):
+    return x * y
+
+dobro = partial(mult,y=2)
